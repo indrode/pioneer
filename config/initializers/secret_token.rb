3096d6efc1ea2ad940311fcfa2748c6dbcbc2155
Indro De <indro.de@gmail.com>
2011-11-03 00:32:37 +0100

    create new rails app without test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pioneer::Application.config.secret_token = 'f64925682e9ef7c7ee95a34bbb595aee1941b68238f6bba0bfe14a27fd473dd63efd065bdbd1e9df3f708003adc7df40e0c1eddfb37f13cc5b66bb3caad1a363'
