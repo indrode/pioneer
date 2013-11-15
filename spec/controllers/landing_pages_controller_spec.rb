8b7bbb745aa190275d624e15d276c9441b03f44f
Indro De <indro.de@gmail.com>
2013-11-15 22:37:45 +0100

    Add a first test

--- spec/controllers/landing_pages_controller_spec.rb
+++ spec/controllers/landing_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe LandingPagesController do
-
+  describe '#index' do
+    it 'should render hello world' do
+      expect(response).to be_success
+      get 'index'
+    end
+  end
 end
