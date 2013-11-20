4bad8a8736092aee6695428d14000ae60c37a744
Indro De <indro.de@gmail.com>
2013-11-20 15:01:49 +0100

    Remove secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Pioneer::Application.config.secret_key_base = '6132a876fcbd2cfa18882be6d6e3883bf0601331c1389362d0d58a94bc4e01bea13627792f2dbd6353c2af1c988b94b0efad608a98f8b15757366eb8467fabd4'
+Pioneer::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
